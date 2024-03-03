81bddd52e8eb9d7db78a809ac6a0c253236b3a15
Andras Milassin <amilassin@gmail.com>
2024-03-03 17:19:39 +0100

    mmkv storage

---
+++ src/components/LogInput/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
+
+export const LogInput = () => {};
--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,43 @@
-import { StateStorage } from 'zustand/middleware'
-import { MMKV } from 'react-native-mmkv'
+import { StateStorage, createJSONStorage, persist } from "zustand/middleware";
+import { MMKV } from "react-native-mmkv";
+import { create } from "zustand";
 
-const storage = new MMKV()
-
-const zustandStorage: StateStorage = {
+const storage = new MMKV();
+const zustandMMKVMiddleware: StateStorage = {
   setItem: (name, value) => {
-    return storage.set(name, value)
+    return storage.set(name, value);
   },
   getItem: (name) => {
-    const value = storage.getString(name)
-    return value ?? null
+    const value = storage.getString(name);
+    return value ?? null;
   },
   removeItem: (name) => {
-    return storage.delete(name)
+    return storage.delete(name);
   },
+};
+
+export type LogEntry = {
+  label: string;
+  timestamp: Date;
+};
+
+export interface LogState {
+  logs: LogEntry[];
+  addLog: (log: LogEntry) => void;
+  removeLog: (log: LogEntry) => void;
 }
+
+export const useLogStore = create<LogState, [["zustand/persist", LogState]]>(
+  persist(
+    (set, get) => ({
+      logs: [],
+      addLog: (log) => set((state) => ({ logs: [...state.logs, log] })),
+      removeLog: (log) => set((state) => ({ logs: state.logs.filter((l) => l !== log) })),
+    }),
+    {
+      name: "logs",
+      storage: createJSONStorage(() => zustandMMKVMiddleware),
+      version: 1,
+    },
+  ),
+);
